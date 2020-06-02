1fc5bdbcf2a45eaa009dde4ecd5eb35d2bf012ce
Glenn Vriesman <glenn.vriesman@gmail.com>
2020-06-02 20:30:18 +0200

    feat: implemented prometheus exporter
    Signed-off-by: Glenn Vriesman <glenn.vriesman@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/glvr182/do-exporter
 
 go 1.14
 
-require github.com/digitalocean/godo v1.37.0
+require (
+	github.com/digitalocean/godo v1.37.0
+	github.com/prometheus/client_golang v1.6.0
+)
--- main.go
+++ main.go
@@ -5,10 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/digitalocean/godo"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	current_bill = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "do_current_bill",
+			Help: "Current bill usage on digitalocean",
+		}, nil,
+	)
+	billing_history = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "do_billing_history",
+			Help: "Past bills on digitalocean",
+		}, []string{"month", "year"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(current_bill, billing_history)
+}
 
 func main() {
 	log.Printf("Starting exporter")
@@ -17,30 +39,49 @@ func main() {
 		log.Fatalln("Please provide an API Key in the environment (DO_KEY)")
 	}
 	client := godo.NewFromToken(key)
-	for {
-		update(client)
-		time.Sleep(time.Minute)
+	go func() {
+		for {
+			if err := update(client); err != nil {
+				log.Fatal(err)
+			}
+			time.Sleep(time.Minute)
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
 }
 
 func update(client *godo.Client) error {
-	bills, resp, err := client.BillingHistory.List(context.Background(), nil)
+	balance, _, err := client.Balance.Get(context.Background())
 	if err != nil {
-		log.Fatalln(err)
+		return err
+	}
+
+	amount, err := strconv.ParseFloat(balance.MonthToDateUsage, 64)
+	if err != nil {
+		return err
+	}
+
+	current_bill.With(prometheus.Labels{}).Set(amount)
+
+	bills, _, err := client.BillingHistory.List(context.Background(), nil)
+	if err != nil {
+		return err
 	}
 
 	for _, bill := range bills.BillingHistory {
 		if bill.Type == "Payment" {
 			continue
 		}
-		log.Printf("%v: %v\n", bill.Date.Format("02-01-2006"), bill.Amount)
+		amount, err := strconv.ParseFloat(bill.Amount, 64)
+		if err != nil {
+			return err
+		}
+		billing_history.With(prometheus.Labels{"year": strconv.Itoa(bill.Date.Year()), "month": bill.Date.Month().String()}).Set(amount)
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Remote returned status: %d\n", resp.StatusCode)
-	}
-
-	balance, resp, err := client.Balance.Get(context.Background())
-	log.Printf("Current bill: %v\n", balance.MonthToDateUsage)
 	return nil
 }
